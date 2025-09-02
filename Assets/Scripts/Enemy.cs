142fe3da090e20023ac53260afca61d8d075e689
DaragonGames <contact@stefan-gerstner.com>
2025-09-02 21:50:47 +0200

    Clean up Terrain

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,25 +2,13 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    float count = 0;
-    Vector3 direction = Vector3.back;
-
-    // Update is called once per frame
     void Update()
     {
-        count += Time.deltaTime;
-        if (count > 2.5f)
-        {
-            direction *= -1;
-            count = 0;
-        }
 
-        transform.position += direction * Time.deltaTime;
     }
 }
